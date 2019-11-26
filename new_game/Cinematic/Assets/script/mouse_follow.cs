e076d0852a6668819f1757c943a8169e088a2180
Quinten  Kempers <83502@b5-400b-30.ict.lab.locals>
2019-11-26 20:59:16 +0100

    sdf test van open avond

---
+++ new_game/Cinematic/Assets/script/mouse_follow.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_follow : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private float sliderX = 15;
+    //private bool isPaused;
+    //public GameObject objectWithScript;
+    //PauseMenu refScript;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //refScript = GameObject.Find("PauseMenuController").GetComponent<PauseMenu>();
+        //GameObject objectWithScript;
+        //objectWithScript.refScript;
+        //objectWithScript.GetComponent<PauseMenu>();
+        Cursor.visible = false;
+        //objectWithScript.isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        if (Input.GetKey(KeyCode.E))
+        {
+            sliderX = 15;
+            transform.eulerAngles = new Vector3(pitch, yaw, -sliderX);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            sliderX = 0;
+            transform.eulerAngles = new Vector3(pitch, yaw, sliderX);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            sliderX = 15;
+            transform.eulerAngles = new Vector3(pitch, yaw, sliderX);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            sliderX = 0;
+            transform.eulerAngles = new Vector3(pitch, yaw, -sliderX);
+        }
+
+    }
+}
+//refScript = GetComponent<PauseMenu>();
+//if (!refScript.isPaused) {
+//Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+// Vector3 pos2 = pos, 1;
+//pos.z += 11;
+// transform.position = pos;
+
+//    Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+//    Vector3 pos = r.GetPoint(1);
+//print(pos);
+//    Vector3 pos2 = pos;
+////pos.z += 11;
+//   // this.transform.position = pos;
+//Debug.Log(pos);
+
+//    var pos = Camera.main.WorldToScreenPoint(transform.position);
+//    var dir = Input.mousePosition - pos;
+//    var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+//    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
---
+++ new_game/Cinematic/Assets/script/move.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float sliderX = 5;
+    public GameObject cameraP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKey(KeyCode.W))
+            {
+            this.transform.GetComponent<Rigidbody>().velocity = new Vector3(-10,0,0);
+            }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.GetComponent<Rigidbody>().velocity = new Vector3(10, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -10);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 10);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            sliderX = 15;
+            cameraP.transform.eulerAngles = new Vector3(sliderX, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            sliderX = 0;
+            cameraP.transform.eulerAngles = new Vector3(sliderX, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            sliderX = 15;
+            sliderX = -sliderX;
+            transform.eulerAngles = new Vector3(sliderX, transform.eulerAngles.y, transform.eulerAngles.z);
+
+            //Vector3 target = new Vector3(40, 2, 27);
+
+            //transform.RotateAround(target, Vector3.left, 30 * Time.deltaTime);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            sliderX = 0;
+            transform.eulerAngles = new Vector3(sliderX, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+    }
+    // }
+}
