db23a785d7a961000e2a72c6d0e87a4026029840
Quinten  Kempers <83502@b5-440-22.ict.lab.locals>
2019-12-05 13:22:18 +0100

    hopen dat git nu niet ******** bestand verwijderd

---
+++ opdracht1 (versie 5.1)/Assets/scripts/BeweegRoteer1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeweegRoteer1 : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    public Text test;
+    public Text test2;
+
+    float turn = 0f;
+
+    float leftRight;
+    float upDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontaal = Input.GetAxis("Horizontal");
+        float verticaal = Input.GetAxis("Vertical");
+
+        transform.position += transform.forward * speed;
+        
+        turn += horizontaal;
+
+        upDown += verticaal;
+        upDown = Mathf.Clamp(upDown, -35 , 35);
+
+        leftRight += horizontaal;
+        leftRight = Mathf.Clamp(leftRight, -35 , 35);
+
+        transform.rotation = Quaternion.Euler(-upDown, turn, -leftRight);
+
+        test.text = horizontaal.ToString();
+        test2.text = verticaal.ToString();
+
+        if (horizontaal < 0.1 && horizontaal > -0.1) {
+             if (leftRight < 0) {
+                leftRight += 0.5f;
+             } else if (leftRight > 0) {
+                leftRight -= 0.5f;
+             }
+         } 
+       
+         if (verticaal == 0) {
+             if (upDown < 0) {
+                upDown += 0.5f;
+             } else if (upDown > 0) {
+                upDown -= 0.5f;
+             }
+         } 
+    }
+}
+
+
+
---
+++ opdracht1 (versie 5.1)/Assets/scripts/beweeg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beweeg : MonoBehaviour
+{
+
+    public float snelheid = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += snelheid * x;
+        pos.y += snelheid * y;
+        pos.z += snelheid;
+        transform.position = pos;
+    }
+}
