3cd565fecfcae428d6323bbb4d37993aac51aae9
Quinten  Kempers <83502@b5-440-21.ict.lab.locals>
2019-12-12 14:00:46 +0100

    oefeningen

---
+++ opdracht2/Assets/script/beweeg.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beweeg : MonoBehaviour
+{
+
+    public float power;
+    private Rigidbody rb;
+
+    Vector3 oldPos;
+    Quaternion oldQ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        power = 350;
+        rb = GetComponent<Rigidbody>();
+
+        oldPos = transform.position;
+        oldQ = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * power);
+            rb.AddTorque(new Vector3(0, power, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //zodat de bal niet meer kan draaien en bewegen
+            rb.freezeRotation = true;
+            rb.velocity = Vector3.zero;
+            //resetten naar normale positie
+            transform.position = oldPos;
+            transform.rotation = oldQ;
+            //zorgen dat de bal weer kan draaien
+            rb.freezeRotation = false;
+        }
+    }
+}
---
+++ opdracht2/Assets/script/cam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+
+    public Transform volgobject;
+    public float afstand = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != volgobject)
+        {
+            Vector3 pos = transform.position;
+            pos.z = volgobject.position.z - afstand;
+            transform.position = pos;
+        }
+    }
+}
