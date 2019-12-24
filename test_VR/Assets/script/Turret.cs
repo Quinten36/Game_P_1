0791ccdc1c49ea95176a0f73ec78e72c4a7dafb9
Quinten36 <codestuff4all@gmail.com>
2019-12-24 22:09:14 +0100

    fatal ontwijken part 2

---
+++ test_VR/Assets/script/Turret.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private Transform target;
+
+    [Header("Attributes")]
+
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Requirements")]
+
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+    public float transSpeed = 7.5f;
+    public GameObject bulletPrefab;
+    //plaats van waar hij schiet. kan een list zijn met zo afwisslende effecten
+    public Transform firePoint;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        //target lock
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * transSpeed).eulerAngles;
+
+        partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot ()
+    {
+        Debug.Log("shoot");
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet bullet = bulletGO.GetComponent<bullet>();
+
+        if (bullet != null)
+            bullet.Seek(target);
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ test_VR/Assets/script/bullet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    private Transform target;
+    public GameObject impactEffect;
+    //public GameObject barrelSmoke;
+    //public Transform effectPoint;
+    public float speed = 70f;
+    //private Turret turret;
+
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //turret = GameObject.Find("Turret").GetComponent<Turret>();
+        //effectPoint = turret.firePoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            //Vector3 dir2 = transform.position;
+            //dir2.y -= Time.deltaTime * 10;
+            //Quaternion down = Quaternion.LookRotation(dir2,dir2);
+            //transform.rotation = down;
+            Debug.Log("werkt");
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+
+    }
+
+    void HitTarget ()
+    {
+        Debug.Log("SDF");
+        GameObject effetctIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        //GameObject smokeIns = (GameObject)Instantiate(barrelSmoke, effectPoint.position, effectPoint.rotation);
+        //Destroy(smokeIns, 1f);
+        Destroy(target.gameObject);
+        Destroy(effetctIns, 2.3f);
+        Destroy(gameObject);
+
+    }
+}
