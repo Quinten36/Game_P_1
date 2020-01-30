8a6db252db2563337d1720117aabb60d6a579efc
Quinten  Kempers <83502@b5-440-23.ict.lab.locals>
2020-01-30 14:53:09 +0100

    enemy mett lopen fix. alleen smooth corners

---
+++ doolhof/Assets/script/goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//moveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class goal : MonoBehaviour
+{
+
+    public Transform goalTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goalTo.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
